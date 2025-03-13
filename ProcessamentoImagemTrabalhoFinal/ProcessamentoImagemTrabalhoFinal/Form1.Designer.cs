eb3131d98f8fff7733cd947deb66a29c9c592d1b
Felipe-Meneguzz1 <lipemeneguzzi@gmail.com>
2025-03-12 22:17:41 -0300

    /feat: Unir duas Imagens

--- ProcessamentoImagemTrabalhoFinal/ProcessamentoImagemTrabalhoFinal/Form1.Designer.cs
+++ ProcessamentoImagemTrabalhoFinal/ProcessamentoImagemTrabalhoFinal/Form1.Designer.cs
@@ -23,13 +23,362 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.bntDown = new System.Windows.Forms.Button();
+            this.bntPlus = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDownBrilhoMenos = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownBrilhoMais = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btImg1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Page2_btn_image1 = new System.Windows.Forms.Button();
+            this.Page2_btn_image2 = new System.Windows.Forms.Button();
+            this.Page2_btn_union = new System.Windows.Forms.Button();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.page2_btn_subtracao = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBrilhoMenos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBrilhoMais)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(873, 699);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.bntDown);
+            this.tabPage1.Controls.Add(this.bntPlus);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.numericUpDownBrilhoMenos);
+            this.tabPage1.Controls.Add(this.numericUpDownBrilhoMais);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.pictureBox3);
+            this.tabPage1.Controls.Add(this.pictureBox2);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.btImg1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(865, 670);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // bntDown
+            // 
+            this.bntDown.Location = new System.Drawing.Point(47, 482);
+            this.bntDown.Name = "bntDown";
+            this.bntDown.Size = new System.Drawing.Size(91, 32);
+            this.bntDown.TabIndex = 25;
+            this.bntDown.Text = "Diminuir";
+            this.bntDown.UseVisualStyleBackColor = true;
+            this.bntDown.Click += new System.EventHandler(this.bntDown_Click);
+            // 
+            // bntPlus
+            // 
+            this.bntPlus.Location = new System.Drawing.Point(47, 407);
+            this.bntPlus.Name = "bntPlus";
+            this.bntPlus.Size = new System.Drawing.Size(91, 32);
+            this.bntPlus.TabIndex = 24;
+            this.bntPlus.Text = "Aumentar";
+            this.bntPlus.UseVisualStyleBackColor = true;
+            this.bntPlus.Click += new System.EventHandler(this.bntPlus_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(42, 454);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 25);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Diminuir Brilho";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(42, 379);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 25);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Aumentar Brilho";
+            // 
+            // numericUpDownBrilhoMenos
+            // 
+            this.numericUpDownBrilhoMenos.Location = new System.Drawing.Point(236, 454);
+            this.numericUpDownBrilhoMenos.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDownBrilhoMenos.Name = "numericUpDownBrilhoMenos";
+            this.numericUpDownBrilhoMenos.Size = new System.Drawing.Size(118, 22);
+            this.numericUpDownBrilhoMenos.TabIndex = 21;
+            // 
+            // numericUpDownBrilhoMais
+            // 
+            this.numericUpDownBrilhoMais.Location = new System.Drawing.Point(236, 382);
+            this.numericUpDownBrilhoMais.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDownBrilhoMais.Name = "numericUpDownBrilhoMais";
+            this.numericUpDownBrilhoMais.Size = new System.Drawing.Size(118, 22);
+            this.numericUpDownBrilhoMais.TabIndex = 20;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(42, 325);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(248, 25);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Alterar Brilho da imagem";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(489, 350);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(269, 198);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 18;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(489, 12);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(269, 198);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 17;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(47, 12);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(269, 198);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btImg1
+            // 
+            this.btImg1.Location = new System.Drawing.Point(107, 613);
+            this.btImg1.Margin = new System.Windows.Forms.Padding(4);
+            this.btImg1.Name = "btImg1";
+            this.btImg1.Size = new System.Drawing.Size(217, 46);
+            this.btImg1.TabIndex = 15;
+            this.btImg1.Text = "Carregar Imagem";
+            this.btImg1.UseVisualStyleBackColor = true;
+            this.btImg1.Click += new System.EventHandler(this.btImg1_Click_1);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.page2_btn_subtracao);
+            this.tabPage2.Controls.Add(this.pictureBox6);
+            this.tabPage2.Controls.Add(this.Page2_btn_union);
+            this.tabPage2.Controls.Add(this.Page2_btn_image2);
+            this.tabPage2.Controls.Add(this.Page2_btn_image1);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.pictureBox5);
+            this.tabPage2.Controls.Add(this.pictureBox4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(865, 670);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox4.Location = new System.Drawing.Point(29, 33);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(269, 198);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 17;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox5.Location = new System.Drawing.Point(29, 383);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(269, 198);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 18;
+            this.pictureBox5.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 16);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Imagem 1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 363);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 16);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Imagem 2";
+            // 
+            // Page2_btn_image1
+            // 
+            this.Page2_btn_image1.Location = new System.Drawing.Point(50, 239);
+            this.Page2_btn_image1.Margin = new System.Windows.Forms.Padding(4);
+            this.Page2_btn_image1.Name = "Page2_btn_image1";
+            this.Page2_btn_image1.Size = new System.Drawing.Size(217, 46);
+            this.Page2_btn_image1.TabIndex = 21;
+            this.Page2_btn_image1.Text = "Carregar Imagem";
+            this.Page2_btn_image1.UseVisualStyleBackColor = true;
+            this.Page2_btn_image1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Page2_btn_image2
+            // 
+            this.Page2_btn_image2.Location = new System.Drawing.Point(50, 589);
+            this.Page2_btn_image2.Margin = new System.Windows.Forms.Padding(4);
+            this.Page2_btn_image2.Name = "Page2_btn_image2";
+            this.Page2_btn_image2.Size = new System.Drawing.Size(217, 46);
+            this.Page2_btn_image2.TabIndex = 22;
+            this.Page2_btn_image2.Text = "Carregar Imagem";
+            this.Page2_btn_image2.UseVisualStyleBackColor = true;
+            this.Page2_btn_image2.Click += new System.EventHandler(this.Page2_btn_image2_Click);
+            // 
+            // Page2_btn_union
+            // 
+            this.Page2_btn_union.Location = new System.Drawing.Point(318, 239);
+            this.Page2_btn_union.Margin = new System.Windows.Forms.Padding(4);
+            this.Page2_btn_union.Name = "Page2_btn_union";
+            this.Page2_btn_union.Size = new System.Drawing.Size(217, 46);
+            this.Page2_btn_union.TabIndex = 23;
+            this.Page2_btn_union.Text = "Somar Imagens";
+            this.Page2_btn_union.UseVisualStyleBackColor = true;
+            this.Page2_btn_union.Click += new System.EventHandler(this.Page2_btn_union_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox6.Location = new System.Drawing.Point(569, 219);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(269, 198);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 24;
+            this.pictureBox6.TabStop = false;
+            // 
+            // page2_btn_subtracao
+            // 
+            this.page2_btn_subtracao.Location = new System.Drawing.Point(318, 333);
+            this.page2_btn_subtracao.Margin = new System.Windows.Forms.Padding(4);
+            this.page2_btn_subtracao.Name = "page2_btn_subtracao";
+            this.page2_btn_subtracao.Size = new System.Drawing.Size(217, 46);
+            this.page2_btn_subtracao.TabIndex = 25;
+            this.page2_btn_subtracao.Text = "Diminuir Imagens";
+            this.page2_btn_subtracao.UseVisualStyleBackColor = true;
+            this.page2_btn_subtracao.Click += new System.EventHandler(this.page2_btn_subtracao_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(869, 708);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBrilhoMenos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBrilhoMais)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button bntDown;
+        private System.Windows.Forms.Button bntPlus;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDownBrilhoMenos;
+        private System.Windows.Forms.NumericUpDown numericUpDownBrilhoMais;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btImg1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Button Page2_btn_image1;
+        private System.Windows.Forms.Button Page2_btn_image2;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Button Page2_btn_union;
+        private System.Windows.Forms.Button page2_btn_subtracao;
     }
 }
 
--- ProcessamentoImagemTrabalhoFinal/ProcessamentoImagemTrabalhoFinal/Form1.cs
+++ ProcessamentoImagemTrabalhoFinal/ProcessamentoImagemTrabalhoFinal/Form1.cs
@@ -16,5 +16,211 @@ namespace ProcessamentoImagemTrabalhoFinal
         {
             InitializeComponent();
         }
+
+        Bitmap img1;
+        Bitmap img2;
+        byte[,] vImg1Gray;
+
+        byte[,] vImg1R;
+        byte[,] vImg1G;
+        byte[,] vImg1B;
+        byte[,] vImg1A;
+        bool bLoadImgOK = false;
+        //Aumentar brilho imagem
+        private void IncreaseBrightnessRGB() {
+            Bitmap image = (Bitmap)pictureBox1.Image;
+            img2 = new Bitmap(image.Width, image.Height);
+            int up = (int)numericUpDownBrilhoMais.Value;
+
+            for (int i = 0; i < image.Width; i++) {
+                for (int j = 0; j < image.Height; j++) {
+                    Color pixel = image.GetPixel(i, j);
+
+                    int R = Math.Min(pixel.R + up, 255);
+                    int G = Math.Min(pixel.G + up, 255);
+                    int B = Math.Min(pixel.B + up, 255);
+
+                    Color newColor = Color.FromArgb(R, G, B);
+
+                    img2.SetPixel(i, j, newColor);
+                }
+            }
+            pictureBox3.Image = img2;
+        }
+
+        private void DecreaseBrightnessRGB() {
+            Bitmap image = (Bitmap)pictureBox1.Image;
+            img2 = new Bitmap(image.Width, image.Height);
+            int down = (int)numericUpDownBrilhoMenos.Value;
+
+            for (int i = 0; i < image.Width; i++) {
+                for (int j = 0; j < image.Height; j++) {
+                    Color pixel = image.GetPixel(i, j);
+
+                    int R = Math.Max(pixel.R - down, 0);
+                    int G = Math.Max(pixel.G - down, 0);
+                    int B = Math.Max(pixel.B - down, 0);
+
+                    Color newColor = Color.FromArgb(R, G, B);
+
+                    img2.SetPixel(i, j, newColor);
+                }
+            }
+
+            pictureBox3.Image = img2;
+        }
+        private void tabPage1_Click(object sender, EventArgs e) {
+
+        }
+
+        private Boolean updateImage() {
+            // Configurações iniciais da OpenFileDialogBox
+            var filePath = string.Empty;
+            openFileDialog1.InitialDirectory = "C:\\Users\\lipem\\OneDrive\\Documentos\\MatLab\\Matlab";
+            openFileDialog1.Filter = "TIFF image (*.tif)|*.tif|JPG image (*.jpg)|*.jpg|BMP image (*.bmp)|*.bmp|PNG image (*.png)|*.png|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+           
+
+            // Se um arquivo foi localizado com sucesso...
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) {
+                // Armnazena o path do arquivo de imagem
+                filePath = openFileDialog1.FileName;
+
+
+                
+                try {
+                    img1 = new Bitmap(filePath);
+                    img2 = new Bitmap(img1.Width, img1.Height);
+                    bLoadImgOK = true;
+                } catch (Exception ex) {
+                    MessageBox.Show(ex.Message, "Erro ao abrir imagem...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    bLoadImgOK = false;
+                }
+            }
+            return bLoadImgOK;
+        }
+
+        private void btImg1_Click_1(object sender, EventArgs e) {
+            Boolean imagemCarregada = updateImage();
+
+            if(imagemCarregada == true) {
+                pictureBox1.Image = img1;
+            }
+
+            // Se a imagem carregou perfeitamente...
+            if (bLoadImgOK == true) {
+                // Adiciona imagem na PictureBox
+                pictureBox1.Image = img1;
+                vImg1Gray = new byte[img1.Width, img1.Height];
+                vImg1R = new byte[img1.Width, img1.Height];
+                vImg1G = new byte[img1.Width, img1.Height];
+                vImg1B = new byte[img1.Width, img1.Height];
+                vImg1A = new byte[img1.Width, img1.Height];
+
+                // Percorre todos os pixels da imagem...
+                for (int i = 0; i < img1.Width; i++) {
+                    for (int j = 0; j < img1.Height; j++) {
+                        Color pixel = img1.GetPixel(i, j);
+
+                        // Para imagens em escala de cinza, extrair o valor do pixel com...
+                        //byte pixelIntensity = Convert.ToByte((pixel.R + pixel.G + pixel.B) / 3);
+                        byte pixelIntensity = Convert.ToByte((pixel.R + pixel.G + pixel.B) / 3);
+                        vImg1Gray[i, j] = pixelIntensity;
+
+                        // Para imagens RGB, extrair o valor do pixel com...
+                        byte R = pixel.R;
+                        byte G = pixel.G;
+                        byte B = pixel.B;
+                        byte A = pixel.A;
+
+                        vImg1R[i, j] = R;
+                        vImg1G[i, j] = G;
+                        vImg1B[i, j] = B;
+                        vImg1A[i, j] = A;
+
+                        Color cor = Color.FromArgb(
+                            255,
+                            vImg1Gray[i, j],
+                            vImg1Gray[i, j],
+                            vImg1Gray[i, j]);
+
+                        img2.SetPixel(i, j, cor);
+                    }
+                }
+                pictureBox2.Image = img2;
+            }
+        }
+
+        private void bntPlus_Click(object sender, EventArgs e) {
+            IncreaseBrightnessRGB();
+        }
+
+        private void bntDown_Click(object sender, EventArgs e) {
+            DecreaseBrightnessRGB();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            Boolean imagemCarregada = updateImage();
+
+            if (imagemCarregada == true) {
+                pictureBox4.Image = img1;
+            }
+
+        }
+
+        private void Page2_btn_image2_Click(object sender, EventArgs e) {
+            Boolean imagemCarregada = updateImage();
+
+            if (imagemCarregada == true) {
+                pictureBox5.Image = img1;
+            }
+
+           
+        }
+
+        private void Page2_btn_union_Click(object sender, EventArgs e) {
+            Bitmap image1 = (Bitmap)pictureBox4.Image;
+            Bitmap image2 = (Bitmap)pictureBox5.Image;
+
+            // Percorre todos os pixels da imagem...
+            for (int i = 0; i < img1.Width; i++) {
+                for (int j = 0; j < img1.Height; j++) {
+                    Color pixel1 = image1.GetPixel(i, j);
+                    Color pixel2 = image2.GetPixel(i, j);
+
+                    int R = Math.Min(pixel1.R + pixel2.R, 255);
+                    int G = Math.Min(pixel1.G + pixel2.G, 255);
+                    int B = Math.Min(pixel1.B + pixel2.B, 255);
+
+                    Color newColor = Color.FromArgb(R, G, B);
+
+                    img2.SetPixel(i, j, newColor);
+                }
+            }
+            pictureBox6.Image = img2;
+        }
+
+        private void page2_btn_subtracao_Click(object sender, EventArgs e) {
+            Bitmap image1 = (Bitmap)pictureBox4.Image;
+            Bitmap image2 = (Bitmap)pictureBox5.Image;
+
+            // Percorre todos os pixels da imagem...
+            for (int i = 0; i < img1.Width; i++) {
+                for (int j = 0; j < img1.Height; j++) {
+                    Color pixel1 = image1.GetPixel(i, j);
+                    Color pixel2 = image2.GetPixel(i, j);
+
+                    int R = Math.Max(pixel1.R - pixel2.R, 0);
+                    int G = Math.Max(pixel1.G - pixel2.G, 0);
+                    int B = Math.Max(pixel1.B - pixel2.B, 0);
+
+                    Color newColor = Color.FromArgb(R, G, B);
+
+                    img2.SetPixel(i, j, newColor);
+                }
+            }
+            pictureBox6.Image = img2;
+        }
     }
 }
